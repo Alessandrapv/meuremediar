26f08b44f3f841b7ba908ae0a2fd66091766e1e3
Athos Schrapett <athosschrapett@outlook.com>
2022-01-13 23:22:04 -0300

    Inclusão Metodo de Cadastro de Pacientes

--- scripts/conexao.js
+++ scripts/conexao.js
@@ -16,6 +16,24 @@ function buscarUsuarios(){
     request.send()
 }
 
+function buscarPacientes(){
+    const request = new XMLHttpRequest()
+
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/posts')
+    request.open('GET', 'http://meuremediar-api-dev.azurewebsites.net/api/v1/Patient')
+
+
+    request.onload = function () {
+        console.log(JSON.parse(this.responseText))
+    }
+
+    request.onerror = function () {
+        console.log('erro ao executar a requisição')
+    }
+
+    request.send()
+}
+
 function createUser(login, password){
     const request = new XMLHttpRequest();
     request.open("POST", "http://meuremediar-api-dev.azurewebsites.net/api/v1/User", true);
@@ -25,3 +43,18 @@ function createUser(login, password){
         password: password
     }));
 }
+
+function createPatient(firstName, lastName, email, contactCel, contactPhone){
+    const request = new XMLHttpRequest();
+    request.open("POST", "http://meuremediar-api-dev.azurewebsites.net/api/v1/Patient", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({
+        firstName: firstName, 
+        lastName: lastName, 
+        email: email, 
+        contactCel: contactCel, 
+        contactPhone: contactPhone
+    }));
+}
+
+
